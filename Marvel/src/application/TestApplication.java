6c94dc77b4ea8037ce0faa702886054461fb3870
Yuri Pourre <yuriamayuri@gmail.com>
2014-03-22 22:00:43 -0300

    Better Dr. Hank Mccoy Animation

--- Marvel/src/application/TestApplication.java
+++ Marvel/src/application/TestApplication.java
@@ -5,7 +5,8 @@ import br.com.etyllica.core.event.GUIEvent;
 import br.com.etyllica.core.event.KeyEvent;
 import br.com.etyllica.core.event.PointerEvent;
 import br.com.etyllica.core.video.Graphic;
-import br.com.etyllica.layer.ImageLayer;
+import br.com.etyllica.layer.AnimatedLayer;
+import br.com.etyllica.layer.BufferedLayer;
 
 public class TestApplication extends Application {
 
@@ -13,14 +14,32 @@ public class TestApplication extends Application {
 		super(w, h);
 	}
 
-	private ImageLayer wolverine;
+	private BufferedLayer wolverine;
+	
+	private AnimatedLayer hank;
 
 	@Override
 	public void load() {
 
-		wolverine = new ImageLayer("sprites/wolverine.png");
+		wolverine = new BufferedLayer(160,260,"sprites/wolverine.png");
+		wolverine.setW(64);
+		wolverine.setH(64);
+		wolverine.flipHorizontal();
+		
+		hank = new AnimatedLayer(40, 40, 64, 96, "sprites/beast.png");
+		hank.setFrames(4);
+		hank.setSpeed(300);
+				
+		updateAtFixedRate(30);
 		
 		loading = 100;
+	}
+	
+	@Override
+	public void timeUpdate(long now){
+		
+		hank.animate(now);
+		
 	}
 	
 	@Override
@@ -28,10 +47,25 @@ public class TestApplication extends Application {
 		
 		wolverine.draw(g);
 		
+		hank.draw(g);
+		
 	}
 	
 	@Override
 	public GUIEvent updateKeyboard(KeyEvent event) {
+		
+		if(event.isKeyDown(KeyEvent.TSK_RIGHT_ARROW)) {
+			hank.setOffsetX(20);
+		} else if(event.isKeyDown(KeyEvent.TSK_LEFT_ARROW)) {
+			hank.setOffsetX(-20);
+		}
+		
+		if(event.isKeyDown(KeyEvent.TSK_DOWN_ARROW)) {
+			hank.setOffsetY(20);
+		} else if(event.isKeyDown(KeyEvent.TSK_UP_ARROW)) {
+			hank.setOffsetY(-20);
+		}
+		
 		// TODO Auto-generated method stub
 		return null;
 	}
